d15feee0227e5be3b7d4afdf87d97442b1f8ba02
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-19 13:45:37 +0100

    Test some repositories

---
+++ test.R
@@ -0,0 +1,10 @@
+unlink(list.files(tempdir(), pattern = '^repos'))
+testrepo <- Sys.getenv('TESTREPO')
+pkgs <- available.packages(repos = testrepo)
+message("Found ", nrow(pkgs), " packages for ", testrepo)
+tryCatch({
+  as.numeric_version(pkgs[,'Version'])
+}, error = function(e){
+  tail(pkgs)
+  stop(e)
+})
